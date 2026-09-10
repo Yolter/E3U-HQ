2e9d5a497e8b1fd515a1450aa984c031a9b56193
Yolter <lossawa1221@gmail.com>
2026-09-10 14:14:58 +0200

    Fix core

--- js/core.js
+++ js/core.js
@@ -1,14 +1,40 @@
-(function(){
+const E3U = {
+  keys: {
+    players: "e3u_players",
+    bank: "e3u_bank",
+    cash: "e3u_cash",
+    truck: "e3u_truck",
+    forum: "e3u_forum",
+    user: "e3u_user",
+    role: "e3u_role"
+  },
 
-if(localStorage.getItem(E3U.keys.user)) return;
+  get(key, fallback) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : fallback;
+  },
 
-const user={
+  set(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
 
-name:"Yolter",
-role:"Founder"
+  players() {
+    return this.get(this.keys.players, []);
+  },
 
+  savePlayers(data) {
+    this.set(this.keys.players, data);
+  }
 };
 
-localStorage.setItem(E3U.keys.user,JSON.stringify(user));
-
+// Первый запуск
+(() => {
+  if (!localStorage.getItem(E3U.keys.players)) {
+    E3U.savePlayers([
+      {
+        name: "Yolter",
+        role: "Founder"
+      }
+    ]);
+  }
 })();
