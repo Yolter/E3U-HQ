0faf41bbaf7175ae8e860118e947fbba850f3fd9
Yolter <lossawa1221@gmail.com>
2026-09-10 14:12:23 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,10 +1,6 @@
-const E3U = {
-  keys:{
-    players:"e3u_players",
-    bank:"e3u_bank",
-    cash:"e3u_cash",
-    truck:"e3u_truck",
-    forum:"e3u_forum",
-    user:"e3u_user",
-    role:"e3u_role"
-  },
+window.E3U = {
+  keys: {
+    user: "e3u_user",
+    lastPage: "e3u_last_page"
+  }
+};
